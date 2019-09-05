897a99fd1695d1fbffd61d0ee58d4bcc13352892
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-04 22:19:08 -0500

    figured out why the computer wasn't retaining the mystery letter between clicks.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,30 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var tries = 0;
+var mysteryLetter;
 
 document.onkeyup = function(event) {
-    // Computer chooses a letter.
-    var mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
-    console.log(mysteryLetter);
-    
     // We record the users choice as a variable.
     var userGuessInput = event.key;
+
+    // Computer chooses a letter.
+    if(tries === 0) {
+        mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+        console.log(mysteryLetter);
+        }
+
     userGuess = userGuessInput.toLowerCase();
     console.log("User guess is " + userGuess);
-    
+        
+    // Check if the letters match.
     if(letters.includes(userGuess)) {
         if(userGuess === mysteryLetter) {
             wins++;
+        } else {
+            tries++;
         }
     }
+
+    console.log("comp choice: "+ mysteryLetter);
     console.log(wins);
 }
