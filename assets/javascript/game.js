4100c161f1789eba5b6c8b3d122ddaee79ac5f6d
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-04 22:35:28 -0500

    fixed a typo in index.html and got the game generally working. Need to figure out how to display it outside the console and get letters guessed working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,14 @@ var wins = 0;
 var losses = 0;
 var tries = 0;
 var mysteryLetter;
+var guessedLetters = [];
 
 document.onkeyup = function(event) {
     // We record the users choice as a variable.
     var userGuessInput = event.key;
 
     // Computer chooses a letter.
-    if(tries === 0) {
+    if (tries === 0) {
         mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
         console.log(mysteryLetter);
         }
@@ -18,14 +19,25 @@ document.onkeyup = function(event) {
     console.log("User guess is " + userGuess);
         
     // Check if the letters match.
-    if(letters.includes(userGuess)) {
-        if(userGuess === mysteryLetter) {
+    if (letters.includes(userGuess)) {
+
+        if (userGuess === mysteryLetter) {
             wins++;
+            tries = 0;
+            guessedLetters = []; 
         } else {
             tries++;
+            guessedLetters.push(userGuess);
+        };
+        if (tries === 9) {
+            tries = 0;
+            losses++;
+            guessedLetters = []; 
         }
     }
 
     console.log("comp choice: "+ mysteryLetter);
-    console.log(wins);
+    console.log("Number of wins: " + wins);
+    console.log("Number of tries: " + tries);
+    console.log("Number of losses " + losses);
 }
