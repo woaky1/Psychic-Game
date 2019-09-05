bf01a4869e2fe99508de73cb258f2002b2c8e093
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-04 21:12:00 -0500

    Got it so the computer will pick a new letter with each key push.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
 var wins = 0;
 var losses = 0;
 var tries = 0;
 
-console.log(mysteryLetter);
-
 document.onkeyup = function(event) {
+    // Computer chooses a letter.
+    var mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(mysteryLetter);
+    
+    // We record the users choice as a variable.
     var userGuessInput = event.key;
     userGuess = userGuessInput.toLowerCase();
     console.log("User guess is " + userGuess);
